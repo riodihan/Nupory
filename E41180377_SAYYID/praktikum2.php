7f0f9af57326dda6ee815ab5d5253ebdf76698f6
mohsaidulmusthofa <sayyidmusthofa89@gmail.com>
2019-09-29 16:37:26 +0700

    Update praktikum2.php

--- E41180377_SAYYID/praktikum2.php
+++ E41180377_SAYYID/praktikum2.php
@@ -1,5 +1,25 @@
 <?php
-$tes = "Saya Pergi Kerja";
-//variabel tes di atas merupakan tipe data string karema
+
+//tipe data string
+$tes = "Saya Pergi Kerja"; 
+
+
+//tipe data integer
+$bilangan_pertama = 12;
+$bilangan_kedua = 78;
+
+//tipe data float
+$angka = 12.177;
+
+//tipe data boolean
+$x = false;
+$y = true;
+
+//tipe data array
+$anggota = array("Andi", "Budi", "Joni");
+
+// menampilkan output
+echo $tes;
+
 
 ?>
