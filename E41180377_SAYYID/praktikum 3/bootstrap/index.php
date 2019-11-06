1dd8f41de16aaac2476d6ab3e577a012c1a71c0b
mohsaidulmusthofa <sayyidmusthofa89@gmail.com>
2019-11-06 23:55:28 +0700

    Update index.php

--- E41180377_SAYYID/praktikum 3/bootstrap/index.php
+++ E41180377_SAYYID/praktikum 3/bootstrap/index.php
@@ -8,33 +8,40 @@
     <h2>LogIn MD5 - www.malasngoding.com</h2>
     <br/>
 
-    <?php
-    if (isset($_GET['pesan'])){
-        if($_GET['pesan'] == "gagal"){
-            echo "LogIn gagal!"
-        }else if($_GET['pesan']=="logOut"){
-            echo "Anda harus LogOut";
-        }elseif ($_GET['pesan'] == "belum_login") {
-            echo "Anda harus LogIn dahulu";
-        }
-    }
-    ?>
-    <br/>
-    <br/>
-    <form method="post" action="cek_login.php">
-        <table>
-            <tr>
-                <td>username</td>
-                <td>:</td>
-                <td><input type="text" name="username" placeholder="masukkan username"></td>     
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td><input type="submit" value="LOGIN"></td>
-            </tr>
-        </table>
-    </form>
+    <?php 
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan'] == "gagal"){
+			echo "Login gagal! username dan password salah!";
+		}else if($_GET['pesan'] == "logout"){
+			echo "Anda telah berhasil logout";
+		}else if($_GET['pesan'] == "belum_login"){
+			echo "Anda harus login untuk mengakses halaman admin";
+		}
+	}
+	?>
+	<br/>
+	<br/>
+	<form method="post" action="cek_login.php">
+		<table>
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input type="text" name="username" placeholder="Masukkan username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input type="password" name="password" placeholder="Masukkan password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td><input type="submit" value="LOGIN"></td>
+			</tr>
+		</table>			
+	</form>
+</body>
+</html>
     
 </body>
 </html>
