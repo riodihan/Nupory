2fbf6f4237a8b92ca7432b2be9a168261a38e8c5
idrisrm <hendrytifa@gmail.com>
2019-11-15 23:33:19 +0700

    datauser

---
+++ project/datauser.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+require 'assets/includes/config.php';
+$user = query("SELECT * FROM user");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Data User</title>
+    <link rel="stylesheet" href="css/styledatauser.css">
+    <link href="https://fonts.googleapis.com/css?family=Be+Vietnam&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=DM+Serif+Display&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+    <style>
+    body{
+        background-image: url('img/Nursery.jpg');
+    }
+    </style>
+</head>
+<body>
+    <header>
+    <div id="hidesidebar" class="hidesidebar">
+        <p class="tombol"> <a href="javascript:void(0)" class="close" onclick="hide()">&#9776;</a></p>
+        <ul>
+            <?php 
+                $user = @$_SESSION['id_status'] == '03';
+                $karyawan = @$_SESSION['id_status'] =='02';
+                $admin = @$_SESSION['id_status'] == '01';
+                $guest = (!isset($_SESSION['login']));
+                
+                if($user){
+            ?>
+                <li><a href="index.php">Beranda</a></li>
+                <li><a href="caraperawatan.php">Cara Perawatan</a></li>
+                <li><a href="kritikdansaran.php">Kritik dan Saran</a></li>
+                <li><a href="temukankami.php">Temukan Kami</a></li>
+                <li><a href="faq.php">FAQ</a></li>
+            <?php }if($admin){?>
+                
+                <li><a href="#">Data Admin</a></li>
+                <li><a href="#">Data Transaksi</a></li>
+                <li><a href="#">Data Bunga</a></li>
+                <li><a href="#">Report</a></li>
+            <?php }if($karyawan){?>
+                
+                <li><a href="#">Data Transaksi</a></li>
+                <li><a href="#">Data Bunga</a></li>
+            <?php }if($guest){?>
+                <li><a href="index.php">Beranda</a></li>
+                <li><a href="caraperawatan.php">Cara Perawatan</a></li>
+                <li><a href="temukankami.php">Temukan Kami</a></li>
+                <li><a href="faq.php">FAQ</a></li>
+            <?php }?>
+        </ul>
+        
+    </div>
+    <div id="menu">
+            <span style="font-size: 30px; cursor: pointer;" onclick="show()">&#9776;</span>
+    </div>
+    <h1 class="h1">Nursery<br>Polije
+        
+        <?php
+        if(!isset($_SESSION["login"])) {?>
+            <button><a href="login.php">Login</a></button>
+        <?php }?>
+
+        <?php  
+        if (isset($_SESSION["login"])) {?> 
+            <button><a href="logout.php">Logout</a></button>
+        <?php }?>
+    </h1>
+    </header>
+    <section>
+        
+        <a style="display:scroll;position:fixed;bottom:0;right:0;" href="https://api.whatsapp.com/send?phone=6281359652164&text=&source=&data=" target="_blank"><input type="image" src="img/WA.png" width="50px" height="50px"></a>
+    </section>
+
+<table border="1" cellpadding="10" cellspacing="0">
+
+    <tr>
+        <th>NO</th>
+        <th>ID Status</th>
+        <th>Nama User</th>
+        <th>Alamat</th>
+        <th>No Telepon</th>
+        <th>Email</th>
+        <th>Username</th>
+        <th>Password</th>
+    </tr>
+
+    <?php $i = 1?>
+    <?php foreach($user as $row) : ?>
+    <tr>
+        <td><?= $i?></td>
+        <td><?= $row["id_status"] ?></td>
+        <td><?= $row["NAMA_USER"] ?></td>
+        <td><?= $row["ALAMAT"] ?></td>
+        <td><?= $row["NO_TELEPON"] ?></td>
+        <td><?= $row["EMAIL"] ?></td>
+        <td><?= $row["USERNAME"] ?></td>
+        <td><?= $row["PASSWORD"] ?></td>
+    </tr>
+    
+    <?php $i++; ?>
+    <?php endforeach;?>
+</table>
+
+
+    <footer>
+    </footer>
+    
+    <script>
+    function show() {
+    document.getElementById("hidesidebar").style.width = "240px";
+    document.getElementById("menu").style.marginLeft = "0%";
+}
+
+    function hide() {
+    document.getElementById("hidesidebar").style.width = "0";
+    document.getElementById("menu").style.marginLeft= "0";
+}
+    </script>
+</body>
+</html>
