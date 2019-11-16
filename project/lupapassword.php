88378bb06a1ab7bb45b96c473f580bf0fac96874
mohsaidulmusthofa <sayyidmusthofa89@gmail.com>
2019-11-16 23:58:44 +0700

    Update lupapassword.php

--- project/lupapassword.php
+++ project/lupapassword.php
@@ -17,7 +17,7 @@ try {
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'sayyidmusthofa89@gmail.com';                     // SMTP username
     $mail->Password   = '@ idul 9';                               // SMTP password
-    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->SMTPSecure = 'ssl';         // Enable ssl encryption;  also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
